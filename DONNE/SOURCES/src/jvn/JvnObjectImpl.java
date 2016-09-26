24486caf9e806ae939678b38a1ecd1b12af8320b
LilyOfTheWest <linovergame@hotmail.fr>
2016-09-26 11:10:53 +0200

    Ajout getters/setters + constructeur dans JvnObjectImpl

--- DONNE/SOURCES/src/jvn/JvnObjectImpl.java
+++ DONNE/SOURCES/src/jvn/JvnObjectImpl.java
@@ -10,6 +10,11 @@ public class JvnObjectImpl implements JvnObject{
 	private int id;
 	private StateLock state;
 	
+	public JvnObjectImpl(Sentence sentence){
+		this.sentence = sentence;
+		this.state = StateLock.NL;
+	}
+	
 	public void jvnLockRead() throws JvnException {
 		state = StateLock.R;
 	}
@@ -40,6 +45,30 @@ public class JvnObjectImpl implements JvnObject{
 
 	public Serializable jvnInvalidateWriterForReader() throws JvnException {		
 		return null;
+	}
+
+	public Sentence getSentence() {
+		return sentence;
+	}
+
+	public void setSentence(Sentence sentence) {
+		this.sentence = sentence;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public StateLock getState() {
+		return state;
+	}
+
+	public void setState(StateLock state) {
+		this.state = state;
 	}
 
 }
